2a4c12671a0b727d33025bb8e820fab4cfa686d8
fiorella maver <maverfiorella26@gmail.com>
2024-02-15 19:42:29 -0300

    evento de sumar y restar

--- react-maver/src/components/ItemDetail.jsx
+++ react-maver/src/components/ItemDetail.jsx
@@ -1,19 +1,22 @@
+import ItemCount from "./ItemCount"
 
 const ItemDetail = ({ item }) => {
 
     return (
-        <div className="container">
-            <div className="producto-detalle">
-                <img src={item.img} alt={item.nombre} />
+    <div className="container">
+        <div className="producto-detalle">
+            <img src={item.img} alt={item.nombre} />
+            <div className="detalle-info">
                 <div>
                     <h3 className="titulo">{item.nombre}</h3>
                     <p className="descripcion">{item.descripcion}</p>
-                    <p className="categoria">Categoría: {item.categoria}</p>
                     <p className="precio">${item.precio}</p>
                 </div>
+                <ItemCount item={item}/>
             </div>
         </div>
-    )
+    </div>
+    );
 }
 
 export default ItemDetail
